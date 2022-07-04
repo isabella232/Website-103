1e183dfa96639526106168cd1c03b8239109c621
Derek Antrican <derekantrican@gmail.com>
2022-07-04 15:03:03 -0700

    Updated download links for 6.3

--- latestVersion.js
+++ latestVersion.js
@@ -1,12 +1,12 @@
 var latestVersionData = {
-    'latestVersion': '6.2',
-    'releaseNotes': 'This version includes a variety of bug fixes including:\r\n- Various crash/startup/file read issues\r\n- Conferencing edit issues (including accounting for the deprecation of Google Hangouts)\r\n- Other minor tweaks & stability improvements\r\n\r\nAnd a small (but often requested) feature: the ability to resize the event list pane!\r\n\r\nAs always, please report any found issues by clicking the bug icon in the program or by sending an email to support@bulkeditcalendarevents.com',
+    'latestVersion': '6.3',
+    'releaseNotes': 'This version includes a variety of bug fixes including:\r\n- Various crash/startup/file read issues\r\n- Other minor tweaks & stability improvements\r\n\r\nAs always, please report any found issues by clicking the bug icon in the program or by sending an email to support@bulkeditcalendarevents.com',
     'updateWin': {
-    'url': 'https://bulk-edit-calendar-events.s3-us-west-2.amazonaws.com/Bulk%20Edit%20Calendar%20Events%20setup%20%5B6.2%5D.exe',
+    'url': 'https://bulk-edit-calendar-events.s3-us-west-2.amazonaws.com/Bulk%20Edit%20Calendar%20Events%20setup%20%5B6.3%5D.exe',
     'size': 37500000
     },
     'updateMac': {
-    'url': 'https://bulk-edit-calendar-events.s3-us-west-2.amazonaws.com/Bulk%20Edit%20Calendar%20Events%20%5B6.2%5D.dmg',
+    'url': 'https://bulk-edit-calendar-events.s3-us-west-2.amazonaws.com/Bulk%20Edit%20Calendar%20Events%20%5B6.3%5D.dmg',
     'size': 37500000
     }
 };
