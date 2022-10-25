7d8426853f07586bcf2f6b6271f58166adb2974b
Derek Antrican <derekantrican@gmail.com>
2022-10-24 21:27:39 -0700

    Update listings for 6.4

--- latestVersion.js
+++ latestVersion.js
@@ -1,12 +1,12 @@
 var latestVersionData = {
-    'latestVersion': '6.3',
+    'latestVersion': '6.4',
     'releaseNotes': 'This version includes a variety of bug fixes including:\r\n- Various crash/startup/file read issues\r\n- Other minor tweaks & stability improvements\r\n\r\nAs always, please report any found issues by clicking the bug icon in the program or by sending an email to support@bulkeditcalendarevents.com',
     'updateWin': {
-    'url': 'https://bulk-edit-calendar-events.s3-us-west-2.amazonaws.com/Bulk%20Edit%20Calendar%20Events%20setup%20%5B6.3%5D.exe',
+    'url': 'https://bulk-edit-calendar-events.s3-us-west-2.amazonaws.com/Bulk%20Edit%20Calendar%20Events%20setup%20%5B6.4%5D.exe',
     'size': 37500000
     },
     'updateMac': {
-    'url': 'https://bulk-edit-calendar-events.s3-us-west-2.amazonaws.com/Bulk%20Edit%20Calendar%20Events%20%5B6.3%5D.dmg',
+    'url': 'https://bulk-edit-calendar-events.s3-us-west-2.amazonaws.com/Bulk%20Edit%20Calendar%20Events%20%5B6.4%5D.dmg',
     'size': 37500000
     }
 };
